4d099d8068fe63175bf42bf79e61d0e5664c8e37
Stuart Van Campen <stuart.vancampen@gmail.com>
2016-01-11 23:07:33 -0500

    friendship updates

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,39 +1,11 @@
 class FriendshipsController < ApplicationController
-  before_action :set_friendship, only: [:show, :update, :destroy]
-
-  # GET /friendships
-  # GET /friendships.json
-  def index
-    @friendships = Friendship.all
-
-    render json: @friendships
-  end
-
-  # GET /friendships/1
-  # GET /friendships/1.json
-  def show
-    render json: @friendship
-  end
 
   # POST /friendships
   # POST /friendships.json
   def create
-    @friendship = Friendship.new(friendship_params)
-
+    @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
     if @friendship.save
       render json: @friendship, status: :created, location: @friendship
-    else
-      render json: @friendship.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /friendships/1
-  # PATCH/PUT /friendships/1.json
-  def update
-    @friendship = Friendship.find(params[:id])
-
-    if @friendship.update(friendship_params)
-      head :no_content
     else
       render json: @friendship.errors, status: :unprocessable_entity
     end
@@ -42,18 +14,9 @@ class FriendshipsController < ApplicationController
   # DELETE /friendships/1
   # DELETE /friendships/1.json
   def destroy
+    @friendship = current_user.friendships.find(params[:id])
     @friendship.destroy
 
     head :no_content
   end
-
-  private
-
-    def set_friendship
-      @friendship = Friendship.find(params[:id])
-    end
-
-    def friendship_params
-      params.require(:friendship).permit(:user_id, :friend_id)
-    end
 end
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,2 +1,4 @@
 class Friendship < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :friend, :className => "User"
 end
