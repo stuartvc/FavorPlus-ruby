530d4114427c15fd82b8a076b66e90c63af83894
Stuart Van Campen <stuart.vancampen@gmail.com>
2016-01-23 15:45:17 -0500

    transaction timestamp

--- app/serializers/transaction_serializer.rb
+++ app/serializers/transaction_serializer.rb
@@ -1,5 +1,5 @@
 class TransactionSerializer < ActiveModel::Serializer
-  attributes :id, :value, :description
+  attributes :id, :value, :description, :created_at
   has_one :user
   has_one :friend
 end
