22895f0f265d9a5bd047eb035152ca819c7c5d12
Stuart Van Campen <stuart.vancampen@gmail.com>
2016-01-23 00:46:30 -0500

    user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,12 +19,22 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(user_params)
-
-    if @user.save
-      render json: { token: @user.auth_token }, status: :created, location: @user
+    if user_params[:email].nil?
+      render json: { error: 'Must provide email'}, status: 400
+    elsif User.exists?(:email => user_params[:email])
+      render json: { error: 'Email already registered'}, status: 400
+    elsif user_params[:firstName].nil?
+      render json: { error: 'Must provide firstName'}, status: 400
+    elsif user_params[:password].nil?
+      render json: { error: 'Must provide password'}, status: 400
     else
-      render json: @user.errors, status: :unprocessable_entity
+      @user = User.new(user_params)
+
+      if @user.save
+        render json: { token: @user.auth_token }, status: :created, location: @user
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
     end
   end
 
